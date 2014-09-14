29bdd37db818829e110d85553c95617b597dfa2a
Brandon Rose <rose.brandon.m@gmail.com>
2014-09-14 18:41:18 -0400

    Update forms.py
    updated to accommodate bootstrap's new form-controls

--- transatlantic/forms.py
+++ transatlantic/forms.py
@@ -5,10 +5,10 @@ from transatlantic.models import Page, Category, UserProfile
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="What do you want to call this story?",
     			widget = forms.TextInput(
-    			attrs = {'size': '75'}))
+    			attrs = {'size': '75', 'class':'form-control', 'placeholder':'What would you like to call your story'}))
     description = forms.CharField(max_length=500, help_text="Give it a brief description.",
     			widget = forms.Textarea(
-    			attrs = {'rows': '6', 'cols':'75'}))		
+    			attrs = {'rows': '6', 'class':'form-control', 'placeholder':'Describe the story'}))		
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
@@ -20,14 +20,14 @@ class CategoryForm(forms.ModelForm):
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="What's the title of your note?",
     			widget = forms.TextInput(
-    			attrs = {'size': '75'}))
-    url = forms.URLField(max_length=200, help_text="Want to link to something?",
-    			widget = forms.TextInput(
-    			attrs = {'size': '75'}))
+    			attrs = {'class':'form-control', 'placeholder':'What would you like to call your note'}))
+    url = forms.URLField(max_length=200,
+    			widget = forms.HiddenInput(),
+    			initial = "http://www.google.com")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     content = forms.CharField(max_length=1000, help_text="Tell the story.",
     			widget = forms.Textarea(
-    			attrs = {'rows': '6', 'cols':'75'}))
+    			attrs = {'rows': '6', 'class':'form-control', 'placeholder':'Here is where you can tell the story'}))
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
@@ -56,13 +56,13 @@ class PageForm(forms.ModelForm):
 class UserForm(forms.ModelForm):
     username = forms.CharField(help_text="Please enter a username.",
         		widget = forms.TextInput(
-    			attrs = {'size': '75'}))
+    			attrs = {'class':'form-control'}))
     email = forms.CharField(help_text="Please enter your email.",
         		widget = forms.TextInput(
-    			attrs = {'size': '75'}))
+    			attrs = {'class':'form-control'}))
     password = forms.CharField(help_text="Please enter a password.",
         		widget = forms.PasswordInput(
-    			attrs = {'size': '75'}))
+    			attrs = {'class':'form-control'}))
 
     class Meta:
         model = User
@@ -71,10 +71,10 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     location = forms.CharField(help_text="Please enter your location or post.", required=False,
         		widget = forms.TextInput(
-    			attrs = {'size': '75'}))
+    			attrs = {'class':'form-control'}))
     bio = forms.CharField(help_text="Tell us about yourself.", required=False,
     			widget = forms.Textarea(
-    			attrs = {'rows': '6', 'cols':'75'}))
+    			attrs = {'rows': '6', 'class':'form-control'}))
     picture = forms.ImageField(help_text="Select a profile image to upload.", required=False)
 
     class Meta:
